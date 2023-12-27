4fdc9f26f421a0190a1061bf523eb8904d2e2c83
Maciej-Milewski1tc <maciejmilewski222@gmail.com>
2023-12-27 19:51:33 +0100

    Removing duplicated code

--- Engine-Editor/src/EditorLayer.cpp
+++ Engine-Editor/src/EditorLayer.cpp
@@ -510,7 +510,6 @@ namespace eg {
 		if (m_SceneState != SceneState::Edit)
 			OnSceneStop();
 
-		//m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
 		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
 
 		m_ActiveScenePath = std::filesystem::path();
@@ -542,7 +541,6 @@ namespace eg {
 		{
 			m_EditorScene = newScene;
 			m_ActiveScene = newScene;
-			m_EditorScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
 			m_SceneHierarchyPanel.SetContext(m_ActiveScene);
 			m_ActiveScenePath = path;
 		}
