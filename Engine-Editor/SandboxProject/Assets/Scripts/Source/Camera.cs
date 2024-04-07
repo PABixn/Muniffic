7afd4b60a4d0083304ae5fc7b1d792653bcb4451
meh <157759635+MichalHenningx@users.noreply.github.com>
2024-04-07 19:31:49 +0200

    sample game

--- Engine-Editor/SandboxProject/Assets/Scripts/Source/Camera.cs
+++ Engine-Editor/SandboxProject/Assets/Scripts/Source/Camera.cs
@@ -19,12 +19,61 @@ namespace Sandbox
 
         void OnCreate()
         {
-
+            m_Transform = GetComponent<TransformComponent>();
+            m_Player = Entity.FindEntityByName("Player");
+            if (m_Player.HasComponent<TextComponent>())
+            {
+                m_Player.AddComponent<TextComponent>();
+                m_Player.GetComponent<TextComponent>().text = "Hello Worldfasdfasdfads!";
+                m_Player.GetComponent<TextComponent>().color = Color.blue;
+            }
         }
 
         void OnUpdate(float ts)
         {
-            
+            if (m_Player != null)
+                m_Transform.translation = new Vector3(m_Player.GetComponent<TransformComponent>().translation.XY, DistanceFromPlayer);
+
+            float speed = 5f;
+            Vector3 velocity = new Vector3(0);
+
+            if (Input.IsKeyDown(KeyCode.Up))
+            {
+                velocity.Y = speed;
+            }
+            else if (Input.IsKeyDown(KeyCode.R))
+            {
+                m_Player.RemoveComponent<TextComponent>();
+            }
+            else if (Input.IsKeyDown(KeyCode.C))
+            {
+                m_Player.AddComponent<TextComponent>();
+            }
+            else if (Input.IsKeyDown(KeyCode.Down))
+            {
+                velocity.Y = -speed;
+            }
+            else
+            {
+                velocity.Y = 0f;
+            }
+            if (Input.IsKeyDown(KeyCode.Left))
+            {
+                velocity.X = -speed;
+            }
+            else if (Input.IsKeyDown(KeyCode.Right))
+            {
+                velocity.X = speed;
+            }
+            else
+            {
+                velocity.X = 0f;
+            }
+
+            Vector3 translation = m_Transform.translation;
+            translation += velocity * ts;
+            m_Transform.translation = translation;
         }
+
     }
 }
