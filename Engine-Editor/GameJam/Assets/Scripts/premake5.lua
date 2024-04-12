faf66330316e9d6f1edc90b151482320b0df2823
Maciej-Milewski1tc <maciejmilewski222@gmail.com>
2024-04-12 14:08:33 +0200

    Creating project

---
+++ Engine-Editor/GameJam/Assets/Scripts/premake5.lua
@@ -0,0 +1,55 @@
+local MunifficRootDir = "../../../../"
+
+include (MunifficRootDir .. "vendor/premake/premake_customization/solution_items.lua")
+
+workspace "Game"
+	architecture "x86_64"
+	startproject "Game"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+project "Game"
+	location "GameJam"
+	kind "SharedLib"
+	language "C#"
+	dotnetframework "4.7.2"
+
+	targetdir ("Binaries")
+	objdir ("Intermediates")
+
+	files
+	{
+		"Source/**.cs",
+		"Properties/**.cs",
+	}
+
+	links
+	{
+		"Muniffic-ScriptCore"
+	}
+
+	filter "configurations:Debug"
+		symbols "Default"
+		optimize "off"
+	
+	filter "configurations:Release"
+		optimize "on"
+		symbols "Default"
+	
+	filter "configurations:Dist"
+		optimize "Full"
+		symbols "Off"
+
+group "Muniffic"
+	include (MunifficRootDir .. "Muniffic-ScriptCore")
+group ""
