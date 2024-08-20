7d746ccec0b91d685504cdfc1a0bd66ed0caf92d
Babicz <stachy225@gmail.com>
2024-08-20 02:12:19 +0200

    Commented logs

--- Engine/src/Platform/Windows/WindowsInput.cpp
+++ Engine/src/Platform/Windows/WindowsInput.cpp
@@ -11,12 +11,12 @@ namespace eg {
 		switch (action)
 		{
 			case GLFW_PRESS:
-				EG_CORE_INFO("Key "+std::to_string(key)+" pressed");
+				// EG_CORE_INFO("Key "+std::to_string(key)+" pressed");
 				for (auto& scriptInstance : ScriptEngine::GetAllScriptInstances())
 					scriptInstance->InvokeOnKeyPress(key);
 				break;
 			case GLFW_RELEASE:
-				EG_CORE_INFO("Key " + std::to_string(key) + " released");
+				// EG_CORE_INFO("Key " + std::to_string(key) + " released");
 				for (auto& scriptInstance : ScriptEngine::GetAllScriptInstances())
 					scriptInstance->InvokeOnKeyRelease(key);
 				break;
@@ -28,12 +28,12 @@ namespace eg {
 		switch (action)
 		{
 			case GLFW_PRESS:
-				EG_CORE_INFO("Mouse " + std::to_string(button) + " pressed");
+				// EG_CORE_INFO("Mouse " + std::to_string(button) + " pressed");
 				for (auto& scriptInstance : ScriptEngine::GetAllScriptInstances())
 					scriptInstance->InvokeOnMouseButtonPress(button);
 				break;
 			case GLFW_RELEASE:
-				EG_CORE_INFO("Mouse " + std::to_string(button) + " released");
+				// EG_CORE_INFO("Mouse " + std::to_string(button) + " released");
 				for (auto& scriptInstance : ScriptEngine::GetAllScriptInstances())
 					scriptInstance->InvokeOnMouseButtonRelease(button);
 				break;
@@ -42,7 +42,7 @@ namespace eg {
 
 	void Input::ScrollCallback(double xOffset, double yOffset)
 	{
-		EG_CORE_INFO("Scroll x:" + std::to_string(xOffset) + " y: " + std::to_string(yOffset));
+		// EG_CORE_INFO("Scroll x:" + std::to_string(xOffset) + " y: " + std::to_string(yOffset));
 		for (auto& scriptInstance : ScriptEngine::GetAllScriptInstances())
 			scriptInstance->InvokeOnScroll(xOffset, yOffset);
 	}
@@ -54,7 +54,7 @@ namespace eg {
 
 	void Input::SetClipboardContent(char* content)
 	{
-		EG_CORE_INFO("Clipboard: " + (std::string)content);
+		// EG_CORE_INFO("Clipboard: " + (std::string)content);
 		glfwSetClipboardString(static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow()), content);
 	}
 
