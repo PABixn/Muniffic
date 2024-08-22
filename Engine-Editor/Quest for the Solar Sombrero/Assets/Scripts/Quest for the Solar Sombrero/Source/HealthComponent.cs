51c6b36b8633a43cf51d16c60c1a17ea2a6ccb41
Maciej-Milewski1tc <maciejmilewski222@gmail.com>
2024-08-22 17:29:14 +0200

    Written Health Component

---
+++ Engine-Editor/Quest for the Solar Sombrero/Assets/Scripts/Quest for the Solar Sombrero/Source/HealthComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest.Source
+{
+    internal class HealthComponent
+    {
+        public int health = 100;
+        public int maxHealth = 100;
+
+        public HealthComponent(int maxHealth) { 
+            this.maxHealth = maxHealth;
+            health = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health < 0)
+            {
+                health = 0;
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            health += healAmount;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+        }
+    }
+}
