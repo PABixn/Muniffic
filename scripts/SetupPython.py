ce380a5c548e007f830aa6b54c10414985e35f6b
Maciej Milewski <98583678+miciex@users.noreply.github.com>
2024-10-01 13:03:50 +0200

    Fixing scripts and deleting projects

--- scripts/SetupPython.py
+++ scripts/SetupPython.py
@@ -8,14 +8,14 @@ class PythonConfiguration:
         if not cls.__ValidatePython():
             return # cannot validate further
 
-        for packageName in ["requests"]:
+        for packageName in ["requests", "openai", "typing", "SpeechRecognition", "pyttsx3", "pyaudio", "python-dotenv"]:
             if not cls.__ValidatePackage(packageName):
                 return # cannot validate further
 
     @classmethod
     def __ValidatePython(cls, versionMajor = 3, versionMinor = 3):
         if sys.version is not None:
-            print("Python version {0:d}.{1:d}.{2:d} detected.".format( \
+            print("Python version {0:d}.{1:d}.{2:d} detected.".format(
                 sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
             if sys.version_info.major < versionMajor or (sys.version_info.major == versionMajor and sys.version_info.minor < versionMinor):
                 print("Python version too low, expected version {0:d}.{1:d} or higher.".format( \
@@ -34,14 +34,14 @@ class PythonConfiguration:
         permissionGranted = False
         while not permissionGranted:
             reply = str(input("Would you like to install Python package '{0:s}'? [Y/N]: ".format(packageName))).lower().strip()[:1]
-            if reply == 'n':
+            if (reply == 'n' or reply == 'N'):
                 return False
-            permissionGranted = (reply == 'y')
+            permissionGranted = (reply == 'y' or reply == 'Y')
         
         print(f"Installing {packageName} module...")
         subprocess.check_call(['python', '-m', 'pip', 'install', packageName])
 
-        return cls.__ValidatePackage(packageName)
+       # return cls.__ValidatePackage(packageName)
 
 if __name__ == "__main__":
     PythonConfiguration.Validate()
