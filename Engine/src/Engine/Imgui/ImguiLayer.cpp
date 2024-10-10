9e9db0e4429c2b1a5b5d3c1543accf603bc53584
Maciej Milewski <98583678+miciex@users.noreply.github.com>
2024-10-10 14:29:55 +0200

    Changed indenting

--- Engine/src/Engine/Imgui/ImguiLayer.cpp
+++ Engine/src/Engine/Imgui/ImguiLayer.cpp
@@ -13,7 +13,7 @@
 
 namespace eg {
 
-    ImGuiLayer::ImGuiLayer()
+	ImGuiLayer::ImGuiLayer()
 		:Layer("ImguiLayer"), m_Time(0.0f)
 	{
 	}
@@ -23,128 +23,128 @@ namespace eg {
 
 	}
 
-	void ImGuiLayer::OnAttach()
-	{
-		EG_PROFILE_FUNCTION();
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO();
+    void ImGuiLayer::OnAttach()
+    {
+        EG_PROFILE_FUNCTION();
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
 
         io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
         io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
         io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
-		ImGui::StyleColorsDark();
-		ImGuiStyle& style = ImGui::GetStyle();
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			style.WindowRounding = 0.0f;
-			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-		}
+        ImGui::StyleColorsDark();
+        ImGuiStyle& style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+        	style.WindowRounding = 0.0f;
+        	style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+        
+        SetDarkThemeColors();
+        
+        Application& app = Application::Get();
+        GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 460");
+    }
 
-		SetDarkThemeColors();
+    void ImGuiLayer::OnDetach()
+    {
+        EG_PROFILE_FUNCTION();
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    }
 
-		Application& app = Application::Get();
-		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
-		ImGui_ImplGlfw_InitForOpenGL(window, true);
-		ImGui_ImplOpenGL3_Init("#version 460");
-	}
-
-	void ImGuiLayer::OnDetach()
-	{
-		EG_PROFILE_FUNCTION();
-		ImGui_ImplOpenGL3_Shutdown();
-		ImGui_ImplGlfw_Shutdown();
-		ImGui::DestroyContext();
-	}
-
-	void ImGuiLayer::OnUpdate(Timestep ts)
-	{
-	}
-
-	void ImGuiLayer::OnImGuiRender()
-	{
-	}
-
-	void ImGuiLayer::OnEvent(Event& e)
-	{
-		if (m_BlockEvents) {
-			ImGuiIO& io = ImGui::GetIO();
-			e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
-			e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
-		}
-	}
-
-	void ImGuiLayer::Begin()
-	{
-		EG_PROFILE_FUNCTION();
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-		ImGuizmo::BeginFrame();
-	}
-
-	void ImGuiLayer::End()
-	{
-		EG_PROFILE_FUNCTION();
-		Application& app = Application::Get();
-		ImGuiIO& io = ImGui::GetIO();
-		io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
-		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
-		ImGui::Render();
-		glViewport(0, 0, app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			GLFWwindow* backup_current_context = glfwGetCurrentContext();
-			ImGui::UpdatePlatformWindows();
-			ImGui::RenderPlatformWindowsDefault();
-			glfwMakeContextCurrent(backup_current_context);
-		}
-	}
-
-	void ImGuiLayer::SetDarkThemeColors()
-	{
-		auto& colors = ImGui::GetStyle().Colors;
-		colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
-
-		// Headers
-		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-
-		// Buttons
-		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-
-		// Frame BG
-		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-
-		// Tabs
-		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
-		colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
-		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-
-		// Title
-		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-	}
-	
-	uint32_t ImGuiLayer::GetActiveWidgetID() const
-	{
-		return GImGui->ActiveId;
-	}
+    void ImGuiLayer::OnUpdate(Timestep ts)
+    {
+    }
+    
+    void ImGuiLayer::OnImGuiRender()
+    {
+    }
+    
+    void ImGuiLayer::OnEvent(Event& e)
+    {
+    	if (m_BlockEvents) {
+    		ImGuiIO& io = ImGui::GetIO();
+    		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+    		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+    	}
+    }
+    
+    void ImGuiLayer::Begin()
+    {
+    	EG_PROFILE_FUNCTION();
+    	ImGui_ImplOpenGL3_NewFrame();
+    	ImGui_ImplGlfw_NewFrame();
+    	ImGui::NewFrame();
+    	ImGuizmo::BeginFrame();
+    }
+    
+    void ImGuiLayer::End()
+    {
+    	EG_PROFILE_FUNCTION();
+    	Application& app = Application::Get();
+    	ImGuiIO& io = ImGui::GetIO();
+    	io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
+    	GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
+    	ImGui::Render();
+    	glViewport(0, 0, app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
+    	ImGui::Render();
+    	int display_w, display_h;
+    	glfwGetFramebufferSize(window, &display_w, &display_h);
+    	glViewport(0, 0, display_w, display_h);
+    	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    
+    	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    	{
+    		GLFWwindow* backup_current_context = glfwGetCurrentContext();
+    		ImGui::UpdatePlatformWindows();
+    		ImGui::RenderPlatformWindowsDefault();
+    		glfwMakeContextCurrent(backup_current_context);
+    	}
+    }
+    
+    void ImGuiLayer::SetDarkThemeColors()
+    {
+    	auto& colors = ImGui::GetStyle().Colors;
+    	colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+    
+    	// Headers
+    	colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+    	colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+    	colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    
+    	// Buttons
+    	colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+    	colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+    	colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    
+    	// Frame BG
+    	colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+    	colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+    	colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    
+    	// Tabs
+    	colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    	colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
+    	colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
+    	colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    	colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+    
+    	// Title
+    	colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    	colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    	colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+    }
+    
+    uint32_t ImGuiLayer::GetActiveWidgetID() const
+    {
+    	return GImGui->ActiveId;
+    }
     
 }
