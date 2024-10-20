31aff644b16a00fc14b24f4e84b5bd496cfe31b2
lunaru100 <lunaruteyvat@gmail.com>
2024-10-20 17:28:47 +0200

    deleted unused code

--- Engine-Editor/src/EditorApp.cpp
+++ Engine-Editor/src/EditorApp.cpp
@@ -9,13 +9,9 @@ Editor::Editor(ApplicationSpecification spec)
     : Application(spec)
     {
         PushLayer(new EditorLayer());
-        GLFWimage images[1];
-        images[0].pixels = stbi_load(LOGO_DIRECTORY, &images[0].width, &images[0].height, 0, 4);
         const char* failReason = stbi_failure_reason();
 
 		SetWindowIcon((GLFWwindow*)this->GetWindow().GetNativeWindow());
-        //glfwSetWindowIcon((GLFWwindow*)this->GetWindow().GetNativeWindow(), 1, images);
-        stbi_image_free(images[0].pixels);
     };
 
 
